24ffa52c2b6eb4f305d0580b6fb051c6190ecc6f
Brad Woodard <bcwoodard@gmail.com>
2016-07-29 13:52:18 -0500

    first commit

---
+++ OraChallenge/ORALoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  ORALoginViewController.h
+//  OraChallenge
+//
+//  Created by Brad Woodard on 7/29/16.
+//  Copyright © 2016 elementC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ORALoginViewController : UIViewController
+
+@end
---
+++ OraChallenge/ORALoginViewController.m
@@ -0,0 +1,71 @@
+//
+//  ORALoginViewController.m
+//  OraChallenge
+//
+//  Created by Brad Woodard on 7/29/16.
+//  Copyright © 2016 elementC. All rights reserved.
+//
+
+#import "ORALoginViewController.h"
+
+@interface ORALoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+- (IBAction)login:(UIButton *)sender;
+
+@end
+
+@implementation ORALoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)login:(UIButton *)sender {
+    NSURL *url = [NSURL URLWithString:@"http://private-d9e5b-oracodechallenge.apiary-mock.com/users/login"];
+    NSDictionary *parameters = @{@"email": _emailTextField.text,
+                                 @"password": _passwordTextField.text};
+    NSData *loginData = [NSJSONSerialization dataWithJSONObject:parameters
+                                                        options:0
+                                                          error:nil];
+    // Create a POST request with our JSON as a request body.
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    request.HTTPMethod = @"POST";
+    request.HTTPBody = loginData;
+    
+    // Create a task.
+    NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithRequest:request
+                                                                 completionHandler:^(NSData *data,
+                                                                                     NSURLResponse *response,
+                                                                                     NSError *error)
+                                  {
+                                      if (!error)
+                                      {
+                                          NSLog(@"Status code: %li", (long)((NSHTTPURLResponse *)response).statusCode);
+                                      }
+                                      else
+                                      {
+                                          NSLog(@"Error: %@", error.localizedDescription);
+                                      }
+                                  }];
+    
+    // Start the task.
+    [task resume];
+}
+@end
